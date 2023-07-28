05378848ae90bd317b7e89c95c5c7aaaa36a3f6c
Alexander Korobov <dktwho@mail.ru>
2023-07-28 18:43:00 +0300

    add navLink and outlet

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
 import React, {useState} from 'react';
 import './App.css';
 import {Site} from "./components/Site";
+import {NavLink, Outlet} from "react-router-dom";
+import styles from "./components/Site.module.css";
 
 
 function App() {
     return (
-        <div>
-            <Site/>
-        </div>
+        <>
+            <div className={styles.header}><h1>HEADER</h1></div>
+            <div className={styles.body}>
+                <div className={styles.nav}>
+                    <div><NavLink to={'/page/0'} className={({isActive}) => isActive ? styles.active : styles.navLink}>PAGE
+                        1</NavLink></div>
+                    <div><NavLink to={'/page/1'} className={({isActive}) => isActive ? styles.active : styles.navLink}>PAGE
+                        2</NavLink></div>
+                    <div><NavLink to={'/page/2'} className={({isActive}) => isActive ? styles.active : styles.navLink}>PAGE
+                        3</NavLink></div>
+                </div>
+                <div className={styles.content}>
+                    <Outlet/>
+                </div>
+            </div>
+        </>
     );
 }
 
