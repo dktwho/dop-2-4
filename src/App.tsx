aab1af053d14e38a0177c539fead725f1f9a7815
Alexander Korobov <dktwho@mail.ru>
2023-07-28 19:28:40 +0300

    create directory helpers, and moved useWindowSize

--- src/App.tsx
+++ src/App.tsx
@@ -3,28 +3,11 @@ import './App.css';
 import {NavLink, Outlet} from "react-router-dom";
 import styles from "./components/Site.module.css";
 import {S} from '../src/styles/_appStyles'
+import {useWindowSize} from "./routes/useWindowSize";
 
 
 function App() {
-
-
     const size = useWindowSize()
-
-    function useWindowSize() {
-        const [windowSize, setWindowSize] = useState(0);
-        useEffect(() => {
-
-            function handleResize() {
-                setWindowSize(window.innerWidth);
-            }
-
-            window.addEventListener("resize", handleResize);
-            handleResize();
-            return () => window.removeEventListener("resize", handleResize);
-        }, []); // Empty array ensures that effect is only run on mount
-        return windowSize;
-    }
-
 
     return (
         <>
